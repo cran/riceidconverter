beb5b556e4089d1748faa80b4c9a82ad062fdd48
Xiang LI <ynaulx@gmail.com>
2020-02-08 08:20:02 +0000

    version 1.1.0

--- R/RiceIDConvert.R
+++ R/RiceIDConvert.R
@@ -8,44 +8,22 @@
 #' @param myID A vector of entrez  id
 #' @param fromType Keytype of input  id
 #' @param toType Keytype of output  id
-#' @param drop A logical vector
 #
 #' @examples 
-#' convert_id <- RiceIDConvert('Os01g0100500','RAP',toType = 'MSU',FALSE)
+#' convert_id <- RiceIDConvert('Os01g0100500','RAP',toType = 'MSU')
 #' convert_id <- RiceIDConvert(myID = 'Os01g0100500',
 #'                             fromType = 'RAP',
-#'                             toType = 'SYMBOL',
-#'                             drop = FALSE)
+#'                             toType = 'SYMBOL')
 #
 #' @export 
 #' 
 #' @return Return a vector or a datafram
-RiceIDConvert <- function(myID,fromType,toType,drop){
-  RiceID_selected <- dplyr::select(riceiddb,fromType,toType)
-  ID_result <- RiceID_selected[which(RiceID_selected == 'Os01g0100500'),]
-  ID_result <- ID_result[-(1:nrow(ID_result)),-(1:ncol(ID_result))]
-
-  for (gene in myID) {
-    if (gene %in% RiceID_selected[,1]) {
-      ID_result_temp <- RiceID_selected[which(RiceID_selected == gene),]
-      #rownames(ID_result_temp) <- paste(gene,seq(1,nrow(ID_result_temp)),sep = '_')
-      colnames(ID_result_temp) <- c(fromType,toType)
-    }else{
-      ID_result_temp <- as.data.frame(t(data.frame(temp = c(gene,rep('No.ID',(length(toType)))))))
-      # rownames(ID_result_temp) <- gene
-      colnames(ID_result_temp) <- c(fromType,toType)
-    }
-    ID_result <- rbind(ID_result,ID_result_temp)
-    if (drop == TRUE | drop == T) {
-      ID_result <- dplyr::filter(ID_result,ID_result[,2] != 'No.ID')
-    }else{
-      ID_result <- ID_result
-    }
-  }
-  if (length(myID) == 1 & nrow(ID_result) < 1) {
-    print('--> No ID can be mapped....')
-  }else{
-    return(unique(ID_result))
-  }
+RiceIDConvert <- function(myID,fromType,toType){
   
+  myid <- as.data.frame(myID)
+  colnames(myid) <- fromType
+  
+  RiceID_selected <- dplyr::select(riceiddb,c(fromType,toType))
+  res <- merge(myid,RiceID_selected, by = fromType, all.x = T)
+  return(res)
 }
